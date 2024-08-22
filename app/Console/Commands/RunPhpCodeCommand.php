7b8d57539effe0c07b57dc6b8dc7f8387bd9dfaa
Dustin Horning <dustinhorning@dustins-m1-wc.mynetworksettings.com>
2024-08-22 13:42:44 -0400

    TASK: Adding basic model and migration and command to get code execution

---
+++ app/Console/Commands/RunPhpCodeCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RunPhpCodeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:run-test-code';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This is a test command to run code';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        
+    }
+}
